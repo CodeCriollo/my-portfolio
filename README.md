a67d500da2f722e130ac531eb2b9a0e348ea2ce6
CodeCriollo <chafe-mute-defiant@duck.com>
2026-09-13 04:53:03 -0300

    Initial commit: Portfolio base setup

---
+++ README.md
@@ -0,0 +1,2 @@
+# my-portfolio
+💻 Personal development portfolio. Here you'll find my main projects, software architecture, database designs, and code experiments. Explore the repositories and feel free to leave a star! ✨
---
+++ script.js
@@ -0,0 +1,14 @@
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+    anchor.addEventListener("click", function (e) {
+        const targetId = this.getAttribute("href");
+        if (targetId && targetId !== "#") {
+            const targetElement = document.querySelector(targetId);
+            if (targetElement) {
+                e.preventDefault();
+                targetElement.scrollIntoView({
+                    behavior: "smooth",
+                });
+            }
+        }
+    });
+});
